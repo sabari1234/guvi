b535970edcef2580fdb1da03bc68dd2b7d280bd7
Manoliya <51356264+Manoliya@users.noreply.github.com>
2019-06-04 17:31:16 +0700

    Create char.py

---
+++ program/char.py
@@ -0,0 +1,7 @@
+char=input()
+if(char=="a" or char=="e" or char=="i" or char=="o" or char=="u"):
+  print("vowel")
+elif(char=="A" or char=="E" or char=="I" or char=="O" or char=="U"):
+  print("vowel")    
+else:
+ print("consonent") 
