0450ee1897727a3ba521fe04f5c70040ce7f69de
Manoliya <51356264+Manoliya@users.noreply.github.com>
2019-06-04 17:32:57 +0700

    Update char.py

--- program/char.py
+++ program/char.py
@@ -2,6 +2,6 @@ char=input()
 if(char=="a" or char=="e" or char=="i" or char=="o" or char=="u"):
   print("vowel")
 elif(char=="A" or char=="E" or char=="I" or char=="O" or char=="U"):
-  print("vowel")    
+  print("Vowel")    
 else:
- print("consonent") 
+  print("Consonant") 
