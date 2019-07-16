893f1a7fa7d5b2c2f4023f1d2871b51c1286d871
Manoliya <51356264+Manoliya@users.noreply.github.com>
2019-07-16 15:15:18 +0530

    Create ex17.py

---
+++ program2/ex17.py
@@ -0,0 +1,17 @@
+Number = int(input())
+Sum = 0
+Times = 0
+Temp = Number
+while Temp > 0:
+           Times = Times + 1
+           Temp = Temp // 10
+
+Temp = Number
+while Temp > 0:
+           Reminder = Temp % 10
+           Sum = Sum + (Reminder ** Times)
+           Temp //= 10
+if Number == Sum:
+           print("yes")
+else:
+           print("no")
