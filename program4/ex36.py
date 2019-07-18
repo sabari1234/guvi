81bba03a437ef926c5e35c8de262b6928c777fc3
Manoliya <51356264+Manoliya@users.noreply.github.com>
2019-07-18 16:22:57 +0530

    Create ex36.py

---
+++ program4/ex36.py
@@ -0,0 +1,8 @@
+ma=input()
+si=0
+for word in ma:
+  
+  if word=='.'or word=='@'or word=='%'or word=='&'or word=='*'or word=='6'or word=='#'or word=='!'or word=='$' or word=='_':
+    si+=1
+  
+print(si) 
