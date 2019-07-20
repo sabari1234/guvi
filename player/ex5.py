cddac49bb21e14e6a53b5edbb2d2377b1564d973
Manoliya <51356264+Manoliya@users.noreply.github.com>
2019-07-20 22:59:40 +0530

    Create ex5.py

---
+++ player/ex5.py
@@ -0,0 +1,7 @@
+a7=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+b7=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+c7=str(input())
+for d7 in range(20):
+  if(c7==a7[d7]):
+    print(b7[d7])
+    break
